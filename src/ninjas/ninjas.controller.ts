d3461cf6523ed5984357d9639ded9644acb6999c
Jameson <jameson@Jamesons-MacBook-Pro.local>
2024-05-31 13:59:59 -0500

    Updating ninja controller

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas?type=fast => []
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+  // GET /ninjas/:id => {}
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return { id };
+  }
+  // POST /ninjas
+  @Post()
+  createNinja(@Body() createNinjaDTO: CreateNinjaDto) {
+    return {
+      name: createNinjaDTO.name,
+    };
+  }
+  // PUT /ninjas/:id => {}
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDTO: CreateNinjaDto) {
+    return { id, name: updateNinjaDTO.name };
+  }
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  deleteNinja(@Param('id') id: string) {
+    return { id };
+  }
+}
